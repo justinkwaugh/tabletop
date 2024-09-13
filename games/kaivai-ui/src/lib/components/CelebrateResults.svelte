0883541be52946491eb00075c21164855522e94f
Justin Waugh <justin@tadpoles.com>
2024-09-13 10:31:15 -0400

    make some descriptions more vertically compact

--- games/kaivai-ui/src/lib/components/CelebrateResults.svelte
+++ games/kaivai-ui/src/lib/components/CelebrateResults.svelte
@@ -12,18 +12,16 @@
 {#if action.metadata?.scores && Object.keys(action.metadata.scores).length > 0}
     <div class="flex flex-row {justifyClass} items-center space-x-2 w-full mt-2 text-md">
         <div class="flex flex-col justify-center items-center">
-            <div>Glory received</div>
             <div
-                class="mt-1 p-2 rounded-lg grid grid-cols-[min-content_min-content_min-content] gap-x-2 gap-y-1 text-center border-2 border-[#634a11]"
+                class="mt-1 p-2 rounded-lg grid grid-cols-[min-content_min-content] gap-x-2 gap-y-1 text-center border-2 border-[#634a11]"
             >
                 {#each gameSession.gameState.players as player}
                     {#if action.metadata?.scores[player.playerId]}
-                        <div class="flex flex-row justify-start items-center">
+                        <div class="flex flex-row justify-end items-center">
                             <PlayerName playerId={player.playerId} />
                         </div>
-                        <div>{'\u2192'}</div>
-                        <div>
-                            {action.metadata?.scores[player.playerId]}
+                        <div class="text-nowrap">
+                            +{action.metadata?.scores[player.playerId]} glory
                         </div>
                     {/if}
                 {/each}
--- games/kaivai-ui/src/lib/components/MoveGodResults.svelte
+++ games/kaivai-ui/src/lib/components/MoveGodResults.svelte
@@ -12,18 +12,16 @@
 {#if action.metadata?.influenceGained && Object.keys(action.metadata.influenceGained).length > 0}
     <div class="flex flex-row {justifyClass} items-center space-x-2 w-full mt-2">
         <div class="flex flex-col justify-center items-center text-md">
-            <div>Influence gained</div>
             <div
-                class="mt-1 p-2 rounded-lg grid grid-cols-[min-content_min-content_min-content] gap-x-2 gap-y-1 text-center border-2 border-[#634a11]"
+                class="mt-1 p-2 rounded-lg grid grid-cols-[min-content_min-content] gap-x-2 gap-y-1 text-center border-2 border-[#634a11]"
             >
                 {#each gameSession.gameState.players as player}
                     {#if action.metadata?.influenceGained[player.playerId]}
-                        <div class="flex flex-row justify-start items-center">
+                        <div class="flex flex-row justify-end items-center">
                             <PlayerName playerId={player.playerId} />
                         </div>
-                        <div>{'\u2192'}</div>
-                        <div>
-                            {action.metadata?.influenceGained[player.playerId]}
+                        <div class="text-nowrap">
+                            +{action.metadata?.influenceGained[player.playerId]} influence
                         </div>
                     {/if}
                 {/each}
