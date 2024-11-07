f111d295574d3b9d9f199bdc7b47bdef05eed981
Justin Waugh <justin@tadpoles.com>
2024-11-07 09:19:48 -0500

    remove unnecessary initializer

--- games/fresh-fish/src/definition/gameInitializer.ts
+++ games/fresh-fish/src/definition/gameInitializer.ts
@@ -1,60 +1,31 @@
-import { GameInitializer, generateSeed, Prng, PrngState, RandomFunction } from '@tabletop/common'
+import {
+    BaseGameInitializer,
+    GameInitializer,
+    generateSeed,
+    Prng,
+    PrngState,
+    RandomFunction
+} from '@tabletop/common'
 import {
     Game,
     Player,
     type HydratedGameState,
     HydratedSimpleTurnManager,
-    shuffle,
-    GameStatus
+    shuffle
 } from '@tabletop/common'
 import { HydratedFreshFishGameState } from '../model/gameState.js'
 import { FreshFishPlayerState, HydratedFreshFishPlayerState } from '../model/playerState.js'
 import { HydratedTileBag, TileBag } from '../components/tileBag.js'
 
 import { nanoid } from 'nanoid'
-import { FreshFishGameConfig, FreshFishGameConfigValidator } from './gameConfig.js'
 import { GoodsType } from './goodsType.js'
 
 import { MachineState } from './states.js'
 import { StallTile, TileType } from '../components/tiles.js'
-import { Value } from '@sinclair/typebox/value'
 import { generateBoard } from '../util/boardGenerator.js'
 import { FreshFishColors } from './colors.js'
 
-export class FreshFishGameInitializer implements GameInitializer {
-    initializeGame(game: Partial<Game>): Game {
-        game.config =
-            game.config ??
-            <FreshFishGameConfig>{
-                numTurnsWithDisksToStart: 3
-            }
-
-        if (!FreshFishGameConfigValidator.Check(game.config)) {
-            throw Error(JSON.stringify([...FreshFishGameConfigValidator.Errors(game.config)]))
-        }
-
-        const newGame: Game = <Game>{
-            id: game.id,
-            isPublic: game.isPublic,
-            status: GameStatus.WaitingForPlayers,
-            typeId: game.typeId,
-            deleted: false,
-            name: game.name?.trim() || 'Fresh Fish',
-            ownerId: game.ownerId,
-            players: game.players,
-            config: game.config,
-            hotseat: false,
-            winningPlayerIds: [],
-            createdAt: new Date() // This will be updated by the db
-        }
-
-        if (!Value.Check(Game, newGame)) {
-            throw Error(JSON.stringify([...Value.Errors(Game, newGame)]))
-        }
-
-        return newGame
-    }
-
+export class FreshFishGameInitializer extends BaseGameInitializer implements GameInitializer {
     initializeGameState(game: Game, seed?: number): HydratedGameState {
         seed = seed === undefined ? generateSeed() : seed
         const prngState: PrngState = { seed, invocations: 0 }
