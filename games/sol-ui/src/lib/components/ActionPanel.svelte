c1ba1aeabe1d5d058b44b6b832498324a6e743ef
Justin Waugh <justin@tadpoles.com>
2024-11-25 10:56:11 -0500

    UI work

--- games/sol-ui/src/lib/components/ActionPanel.svelte
+++ games/sol-ui/src/lib/components/ActionPanel.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import { getContext } from 'svelte'
     import type { SolGameSession } from '$lib/model/SolGameSession.svelte'
+    import MoveArrows from '$lib/images/movearrows.svelte'
+    import ConvertAtom from '$lib/images/convertatom.svelte'
+    import ActivateBolt from '$lib/images/activatebolt.svelte'
     import { MachineState } from '@tabletop/sol'
     import { Button } from 'flowbite-svelte'
     import { slide, fade } from 'svelte/transition'
@@ -33,9 +36,31 @@
 </script>
 
 <div
-    class="mb-2 rounded-lg bg-gray-300 p-2 text-center flex flex-row flex-wrap justify-center items-center"
+    class="sol-font mb-2 rounded-lg p-2 flex flex-row flex-wrap justify-center items-center gap-x-4 text-[#ad9c80]"
 >
-    <div class="flex flex-col justify-center items-center mx-8">
+    <!-- <div class="flex flex-col justify-center items-center mx-8">
         <h1 class="text-lg">{instructions}</h1>
+    </div> -->
+    <div class="flex sol-font-bold me-4 leading-tight text-center">CHOOSE<br />AN ACTION</div>
+    <div class="flex">
+        <button
+            class="box-border h-[52px] flex items-center justify-center p-2 px-4 bg-transparent hover:border hover:border-[#ad9c80] rounded-lg sol-font-bold"
+            ><MoveArrows />
+            <div class="ms-3">MOVE</div></button
+        >
+    </div>
+    <div class="flex">
+        <button
+            class="opacity-50 box-border h-[52px] flex items-center justify-center p-2 px-4 bg-transparent hover:border hover:border-[#ad9c80] rounded-lg sol-font-bold"
+            ><ConvertAtom />
+            <div class="ms-3">CONVERT</div></button
+        >
+    </div>
+    <div class="flex">
+        <button
+            class="opacity-50 box-border h-[52px] flex items-center justify-center p-2 px-4 bg-transparent hover:border hover:border-[#ad9c80] rounded-lg sol-font-bold"
+            ><ActivateBolt />
+            <div class="ms-3">ACTIVATE</div></button
+        >
     </div>
 </div>
