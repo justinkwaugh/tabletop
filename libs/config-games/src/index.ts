04e63d63a0015dbb739167caa9009c44bc7caca7
Justin Waugh <justin.w@teachingstrategies.com>
2025-11-04 16:14:39 +0000

    more config changes

--- libs/config-games/src/index.ts
+++ libs/config-games/src/index.ts
@@ -3,7 +3,7 @@ import gamesJson from './games.json'
 type GamePackage = [name: string, scope?: string]
 
 function loadGamePackages(): GamePackage[] {
-    const packages: GamePackage[] = gamesJson.games.map((fullName) => {
+    const packages: GamePackage[] = gamesJson.availableGames.map((fullName) => {
         const parts = fullName.split('/')
         if (parts.length === 2) {
             return [parts[1], parts[0]] as GamePackage
