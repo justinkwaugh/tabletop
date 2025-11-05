aeb62eff0c03a4b1960c39372bbb6f4473c251d6
Justin Waugh <justin.w@teachingstrategies.com>
2025-11-05 16:04:48 +0000

    add vite plugin

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,20 +1,12 @@
+import devtoolsJson from 'vite-plugin-devtools-json'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        fs: {
-            strict: false
-        }
-    },
-    plugins: [sveltekit()],
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
-    },
+    server: { host: '0.0.0.0', fs: { strict: false } },
+    plugins: [sveltekit(), devtoolsJson()],
+    test: { include: ['src/**/*.{test,spec}.{js,ts}'] },
     build: {
-        commonjsOptions: {
-            include: [/node_modules/]
-        }
+        commonjsOptions: { include: [/node_modules/] }
     }
 })
