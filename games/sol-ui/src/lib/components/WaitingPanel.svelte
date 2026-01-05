e36df35f8fcdbd661f85b843c13ff53f1f67fc04
Justin Waugh <justin.w@teachingstrategies.com>
2026-01-04 21:48:04 -0500

    Refresh waiting panel text
    Use PlayerName and game state to build the waiting message.

--- games/sol-ui/src/lib/components/WaitingPanel.svelte
+++ games/sol-ui/src/lib/components/WaitingPanel.svelte
@@ -2,42 +2,32 @@
     import { getContext } from 'svelte'
     import type { SolGameSession } from '$lib/model/SolGameSession.svelte'
     import { fade } from 'svelte/transition'
+    import { PlayerName } from '@tabletop/frontend-components'
 
     let gameSession = getContext('gameSession') as SolGameSession
-
-    let waitingText = $derived.by(() => {
+    let activePlayerId = $derived.by(() => {
         if (gameSession.activePlayers.length === 0) {
-            return ''
+            return
         }
-
-        const activePlayer = gameSession.game.players.find(
-            (player) => player.id === gameSession.activePlayers[0].id
-        )
-
-        if (!activePlayer) {
-            return ''
-        }
-
-        if (gameSession.isDrawingCards) {
-            return `WAITING FOR ${activePlayer.name} TO DRAW ${gameSession.myPlayerState?.drawnCards.length ?? 0} CARD${(gameSession.myPlayerState?.drawnCards.length ?? 1) === 1 ? '' : 'S'}`
-        } else if (
-            gameSession.isActivating &&
-            gameSession.myPlayerState &&
-            gameSession.myPlayerState.playerId !== gameSession.turnPlayer?.playerId
-        ) {
-            return 'WAITING FOR ' + activePlayer.name + ' TO DECIDE ABOUT THE BONUS'
-        } else {
-            return 'WAITING FOR ' + activePlayer.name
-        }
+        return gameSession.activePlayers[0].id
     })
 </script>
 
 <div
     in:fade={{ duration: 300, delay: 100 }}
     out:fade={{ duration: 100 }}
-    class="ms-3 py-2 flex flex-row justify-center items-center h-[50px] {waitingText
-        ? ''
-        : 'pointer-events-none'}"
+    class="ms-3 py-2 flex flex-row justify-center items-center h-[50px] sol-font"
 >
-    <div class="me-2">{waitingText}</div>
+    <div class="me-2">
+        WAITING FOR <PlayerName
+            playerId={activePlayerId}
+            additionalClasses={'pt-[2px]'}
+            capitalization={'uppercase'}
+        />
+        {#if gameSession.isDrawingCards}
+            TO DRAW CARDS
+        {:else if gameSession.isActivating && gameSession.myPlayerState && gameSession.myPlayerState.playerId !== gameSession.activePlayers[0]?.id}
+            TO DECIDE ABOUT THE BONUS
+        {/if}
+    </div>
 </div>
