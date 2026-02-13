0b695e31c0d85a023120ff87a3c2b95d8f635bd2
Justin Waugh <justin.w@teachingstrategies.com>
2026-02-13 09:39:18 -0500

    fix css

--- games/bus-ui/src/lib/definitions/definition.ts
+++ games/bus-ui/src/lib/definitions/definition.ts
@@ -1,7 +1,7 @@
 import type { GameUiDefinition } from '@tabletop/frontend-components/definition/gameUiDefinition'
 import { BusInfo } from '@tabletop/bus'
 import type { BusGameState, HydratedBusGameState } from '@tabletop/bus'
-import coverImg from '$lib/images/bus_cover.webp'
+import coverImg from '$lib/images/bus_cover.jpg'
 
 export const UiDefinition: GameUiDefinition<BusGameState, HydratedBusGameState> = {
     info: {
