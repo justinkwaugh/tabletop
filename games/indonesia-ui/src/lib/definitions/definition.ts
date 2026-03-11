2f72ec8494186f781cb0f75c0f5d0707135f6eb5
Justin Waugh <justin.w@teachingstrategies.com>
2026-03-11 12:28:02 -0400

    cleanup

--- games/indonesia-ui/src/lib/definitions/definition.ts
+++ games/indonesia-ui/src/lib/definitions/definition.ts
@@ -1,7 +1,7 @@
 import type { GameUiDefinition } from '@tabletop/frontend-components/definition/gameUiDefinition'
 import { IndonesiaInfo } from '@tabletop/indonesia'
 import type { IndonesiaGameState, HydratedIndonesiaGameState } from '@tabletop/indonesia'
-import coverImg from '$lib/images/cover.jpg'
+import coverImg from '$lib/images/indonesia_cover.jpg'
 
 export const UiDefinition: GameUiDefinition<IndonesiaGameState, HydratedIndonesiaGameState> = {
     info: {
