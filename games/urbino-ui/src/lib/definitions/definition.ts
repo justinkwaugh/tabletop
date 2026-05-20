9a7d10468fde564754b0cebb1962c2a1bb56468e
Jeremiah Lee <jeremiah.lee@gmail.com>
2026-05-20 12:23:37 -0700

    [urbino] add logo image

--- games/urbino-ui/src/lib/definitions/definition.ts
+++ games/urbino-ui/src/lib/definitions/definition.ts
@@ -1,11 +1,12 @@
 import type { GameUiDefinition } from '@tabletop/frontend-components/definition/gameUiDefinition'
 import { UrbinoInfo } from '@tabletop/urbino'
 import type { UrbinoGameState, HydratedUrbinoGameState } from '@tabletop/urbino'
+import coverImg from '$lib/images/urbino-cover.png'
 
 export const UiDefinition: GameUiDefinition<UrbinoGameState, HydratedUrbinoGameState> = {
     info: {
         ...UrbinoInfo,
-        thumbnailUrl: ''
+        thumbnailUrl: coverImg
     },
     runtime: async () => {
         return (await import('./runtime.js')).UrbinoUiRuntime
